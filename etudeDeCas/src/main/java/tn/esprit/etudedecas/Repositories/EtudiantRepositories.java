0f7745d0d273b5801c56e477baad74777591f4c0
Baha Rojbi <101024667+Baha-Rojbi@users.noreply.github.com>
2023-11-16 23:48:42 +0100

    Reservation Repo
    just changed the reservation Repo Headers

--- etudeDeCas/src/main/java/tn/esprit/etudedecas/Repositories/EtudiantRepositories.java
+++ etudeDeCas/src/main/java/tn/esprit/etudedecas/Repositories/EtudiantRepositories.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import tn.esprit.etudedecas.entities.Etudiant;
+import tn.esprit.etudedecas.entities.Reservation;
 
 import java.util.Date;
 import java.util.List;
@@ -13,4 +14,6 @@ public List<Etudiant> findByNomEt(String nom);
 @Query("SELECT e from Etudiant e where e.nomEt=:nom")
     public List<Etudiant> getEtudiantByNom(@Param("nom") String nom);
 public List<Etudiant> findByDateNaissanceBetween(Date dateDebut,Date dateFin);
+
+
 }
--- etudeDeCas/src/main/java/tn/esprit/etudedecas/Repositories/ReservationRepositories.java
+++ etudeDeCas/src/main/java/tn/esprit/etudedecas/Repositories/ReservationRepositories.java
@@ -1,7 +1,15 @@
 package tn.esprit.etudedecas.Repositories;
 
+import jdk.dynalink.linker.LinkerServices;
 import org.springframework.data.jpa.repository.JpaRepository;
+import tn.esprit.etudedecas.entities.Etudiant;
 import tn.esprit.etudedecas.entities.Reservation;
 
+import java.util.List;
+
 public interface ReservationRepositories extends JpaRepository<Reservation,Long> {
+ public List<Reservation> findByEtudiantsByIdEtudiant(Long idEtudiant);
+
+ public List<Reservation> findByEtudiantsNomEt(String nomEt);
+ public List<Reservation>  findByEtudiantNomEtStartingWith(String prefix);
 }
